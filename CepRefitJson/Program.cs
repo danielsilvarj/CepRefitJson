868137dd2485bef78954274e203db5623a7488f7
danielsilvarj <daniel.cg.rio@gmail.com>
2020-03-20 14:28:06 -0300

    Subindo Projeto.

--- CepRefitJson/Program.cs
+++ CepRefitJson/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
 
 namespace CepRefitJson
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+
+            Console.WriteLine("Digite o Cep para Buscar");
+            string meuCep = Console.ReadLine();
+
+            Console.WriteLine("Consultando dados do Cep:" + meuCep);
+
+            var address = await cepClient.GetAddressAsync(meuCep);
+
+            Console.Write($"\nLogradouro:{address.Logradouro}\nBairro:{address.Bairro}" +
+                $"\nEstado:{address.Uf}\nCódigo Ibge:{address.Ibge}");
+            Console.ReadKey();
         }
     }
 }
